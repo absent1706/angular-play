c47ee570a3114cd9d2d63ce944edff63246d436c
Alexander Litvinenko <litvinenko1706@gmail.com>
2016-02-15 21:49:52 +0200

    3.2

--- app.js
+++ app.js
@@ -24,6 +24,16 @@
     };
   });
 
+  app.controller('ReviewController', function(){
+    this.review = {};
+
+    this.addReview = function (product) {
+      product.reviews.push(this.review)
+      this.review = {};
+    }
+  });
+
+
   var gems = [
     {
       name: 'Azurite',
