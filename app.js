eb0491252ba8ea48e6b6df87dcf748cc77fb58a4
Alexander Litvinenko <litvinenko1706@gmail.com>
2016-02-15 22:47:19 +0200

    4.after_challeges

--- app.js
+++ app.js
@@ -28,16 +28,23 @@
     };
   });
 
-  app.controller('StoreController', function() {
-    this.products = gems;
+  app.directive('productGallery', function() {
+    return {
+      restrict: 'E', // element
+      templateUrl: 'product-gallery.html',
+      controller: function() {
+        this.current = 0;
+
+        this.setCurrent = function(imageNumber){
+          this.current = imageNumber || 0;
+        };
+      },
+      controllerAs: 'gallery'
+    };
   });
 
-  app.controller('GalleryController', function(){
-    this.current = 0;
-
-    this.setCurrent = function(imageNumber){
-      this.current = imageNumber || 0;
-    };
+  app.controller('StoreController', function() {
+    this.products = gems;
   });
 
   app.controller("ReviewController", function(){
